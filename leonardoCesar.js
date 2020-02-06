3461bb86f3cdec4a90fa856b83d3472e54cbf1a7
LeoLCM <mendonca_leonardo@hotmail.com>
2020-02-06 17:54:21 -0200

    adicionando meu arquivo.

---
+++ leonardoCesar.js
@@ -0,0 +1,6 @@
+let status = {
+    nome: "Leonardo Cesar de Mendonça",
+    passatempo: "Jogar video-game",
+    esporte: "Nenhum",
+    membros: ["Leonardo Cesar", "Leandro Brotas","Andreia Zutin"]
+};
