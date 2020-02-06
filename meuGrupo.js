6a9ecf3374edcf2fb96ad7ed34e63ecddf18a9af
LeoLCM <mendonca_leonardo@hotmail.com>
2020-02-06 18:03:40 -0200

    Adicionando o meuGrupo.js

---
+++ meuGrupo.js
@@ -0,0 +1,18 @@
+let grupo = [{
+    nome: "Leandro",
+    passatempo: "Futebol",
+    esporte: "Futebol",
+    membros: ["Leandro Brotas","Leonardo Cesar","Andreia Zutin"]
+},
+{
+    nome: "Leonardo Cesar de Mendonça",
+    passatempo: "Jogar video-game",
+    esporte: "Nenhum",
+    membros: ["Leonardo Cesar", "Leandro Brotas","Andreia Zutin"]
+},
+{
+    nome: "Andreia Zutin",
+    passatempo: "Artesanato",
+    esporte: "Corrida de bengala",
+    membros: ["Leonardo Cesar", "Leandro Brotas","Andreia Zutin"]
+}]
